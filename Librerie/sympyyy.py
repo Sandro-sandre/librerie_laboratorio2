dad8ca3ec1e99f5ed633b2837301baa18c516d0d
livio <liviolando@gmail.com>
2025-02-25 21:04:09 +0000

    aggiunta file sympy

---
+++ Librerie/sympyyy.py
@@ -0,0 +1,31 @@
+from sympy import *
+import sys 
+import numpy as np
+
+def error_propagation(formula, variables, errors):
+    '''
+    calcola propagazione degli errori con formula data 
+    formula deve essere simbolica 
+    errors: lista degli errori associate alle variabili
+    variabili lista delle variabili 
+    TUTTE LE LISTE DEVONO essere in stringhe tranne quella degli errori
+    '''
+    sym_variables = [symbols(var) for var in variables]
+    expr = sympify(formula)
+    partial_derivates = [diff(expr, var) for var in sym_variables]
+    
+    error_squared = np.sum((deriv*error)**2 for deriv, error in zip(partial_derivates, errors))
+    errore_propagato = sqrt(error_squared)
+    return errore_propagato.evalf(4)
+
+def main():
+    
+    formula = "x+y"
+    variables = ["x", "y"]
+    errors = [1, 2]
+
+    result = error_propagation(formula, variables, errors)
+    print(f"errore propagato sulla formula'{formula}': {result}")
+
+if __name__ == "__main__" :
+    main()
